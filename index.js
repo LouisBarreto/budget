c192ccef8541efeda7dd8fb143e8c294cb1190e7
Louis B <barretolouis5@gmail.com>
2022-09-13 23:34:38 -0500

    changed index.js and index.html. Added packages

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
 let weeklyExpenses = 0
 let monthlyExpenses = 0
 let annualExpenses = 0
+    
+//Weekley Expense for Loop
+for (let i = 0; i < weeklyExpenseQuestions.length; i++){
+    let stringAnswer = window.prompt(weeklyExpenseQuestions[i]);
+    let numberAnswer = parseFloat(stringAnswer); 
+    console.log(numberAnswer);
+    weeklyExpenses = weeklyExpenses + numberAnswer;
+}
 
-// Your Code Here
+//Monthly Expense for loop
+for (let i = 0; i < monthlyExpenseQuestions.length; i++){
+    let stringAnswer = window.prompt(monthlyExpenseQuestions[i]);
+    let numberAnswer = parseFloat(stringAnswer); 
+    console.log(numberAnswer);
+    monthlyExpenses = monthlyExpenses + numberAnswer;
+}
+
+// Annual Expense for loop
+
+for (let i = 0; i<annualExpensesQuestions.length; i++){
+    let stringAnswer = window.prompt(annualExpenseQuestions[i]);
+    let numberAnswer = parseFloat(stringAnswer);
+    console.log(numberAnswer);
+    annualExpenses = annualExpenses + numberAnswer;
+
+}
